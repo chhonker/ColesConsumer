993360170f9df933a00ad5863ed59a104c5ac1c0
Sumit Chhonker <chhonker.sumit@gmail.com>
2021-09-02 17:08:38 +0530

    Initial Commit

---
+++ src/main/java/io/sumit/kafka/ColesConsumer/ColesConsumerApplication.java
@@ -0,0 +1,13 @@
+package io.sumit.kafka.ColesConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColesConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColesConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sumit/kafka/ColesConsumer/ColesConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sumit.kafka.ColesConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColesConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
